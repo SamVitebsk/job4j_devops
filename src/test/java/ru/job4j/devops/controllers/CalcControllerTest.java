2f1b2be5626a8cf3b2bfc13071f3a659dd67d383
a.samusev <sam.vitebsk@gmail.com>
2025-08-08 00:14:56 +0300

    changed test

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -28,9 +28,9 @@ class CalcControllerTest {
     }
 
     @Test
-    public void whenZeroPlusZero() {
-        var input = new TwoArgs(0, 3);
-        var expected = new Result(3);
+    public void whenZeroPlusFour() {
+        var input = new TwoArgs(0, 4);
+        var expected = new Result(4);
         var output = new CalcController().summarise(input);
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
